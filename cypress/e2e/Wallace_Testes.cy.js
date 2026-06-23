34f7528e931333fa63f4f076df0f864d234af100
wallcoto <wallacecicero3280@gmail.com>
2026-06-23 19:56:12 -0300

    Primeiros testes

--- cypress/e2e/Wallace_Testes.cy.js
+++ cypress/e2e/Wallace_Testes.cy.js
@@ -1,5 +1,14 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Wallace_Testes', () => {
+ beforeEach(() => {
+   cy.visit('./src/Index.html')
+ })
+it('Verifica o titulo da pagina', () => {
+  cy.title()
+    .should('be.equal', 'Praticando Testes')
+  })
+
+it.only('Verifica o texto do titulo', () => {
+  cy.get('#title')
+    .should('have.text', 'Praticando Testes')
   })
 })
